e2184c8f270ee9bf7d9c4c8a4feb92932c518582
Eddie Rangel <hacker@eddierangel.com>
2016-02-07 12:03:07 -0800

    Adding a stop watch and setting up a one minute timer.

---
+++ PrimeInSixtySeconds/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimInSixtySeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            TimeSpan running = new TimeSpan();
+
+            var sixtySeconds = 60;
+
+            timer.Start();
+            for (int i = 0; running.Minutes < 1;i++)
+            {
+                System.Console.WriteLine("number: {0}", i);
+                running = timer.Elapsed;
+            }
+            timer.Stop();
+            System.Console.Read();
+        }
+    }
+}
