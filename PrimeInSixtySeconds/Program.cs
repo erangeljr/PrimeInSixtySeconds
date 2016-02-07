1b1a47dbb19d032c7f032e42f18a1e7a45ce5339
Eddie Rangel <hacker@eddierangel.com>
2016-02-07 13:10:07 -0800

    Calculating prime

--- PrimeInSixtySeconds/Program.cs
+++ PrimeInSixtySeconds/Program.cs
@@ -1,29 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Eddie Rangel | Get the highest Prime in 60 Seconds
+*/
 
-namespace PrimInSixtySeconds
+using System;
+using System.Diagnostics;
+
+namespace PrimeInSixtySeconds
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stopwatch timer = new Stopwatch();
-            TimeSpan running = new TimeSpan();
+            System.Console.WriteLine("Calculating highest Prime in 60 seconds...");
+            SixtySecondsStartsNow();
+        }
 
-            var sixtySeconds = 60;
+        private static void SixtySecondsStartsNow()
+        {
+            Stopwatch timer = new Stopwatch();
+            var max = 0;
 
             timer.Start();
-            for (int i = 0; running.Minutes < 1;i++)
-            {
-                System.Console.WriteLine("number: {0}", i);
-                running = timer.Elapsed;
+            for (int i = 0; timer.Elapsed.Minutes < 1; i++)
+            {                
+                if (CheckPrime(i))
+                {
+                    max = i;
+                    //System.Console.ForegroundColor = ConsoleColor.Green;
+                    //System.Console.WriteLine("Prime: {0}",max);
+                }
+                //System.Console.ForegroundColor = ConsoleColor.Gray;
+                //System.Console.WriteLine("{0} seconds", timer.Elapsed.Seconds);                
             }
             timer.Stop();
+            System.Console.WriteLine("The highest prime number I calculated in 60 seconds is: {0}", max);
             System.Console.Read();
+        }
+
+        static bool CheckPrime(int number)
+        {
+            var lowerBound = Math.Floor(Math.Sqrt(number));
+
+            if (number == 1)
+                return false;
+            if (number == 2)
+                return true;
+
+            for (int i = 2; i <= lowerBound; i++)
+            {
+                //System.Console.WriteLine("Number: {0}, LowerBound: {1}", i, lowerBound);
+                if (number % i == 0) return false;
+            }
+
+            return true;
+
         }
     }
 }
